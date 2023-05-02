29b3cea8b06258c415fb5c2c6c13d539e4f102a2
ronit0717 <ronit0717@gmail.com>
2023-05-03 00:38:12 +0530

    Footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,15 @@
 import React from "react"
 import SocialLinks from "../constants/socialLinks"
 const Footer = () => {
-  return <h2>footer component</h2>
+  return <footer className="footer">
+    <div>
+      <SocialLinks styleClass="footer-links" />
+      <h4>
+        copyright&copy;{new Date().getFullYear()} 
+        <span>Ronit Chattopadhyay</span> all rights reserved
+      </h4>
+    </div>
+  </footer>
 }
 
 export default Footer
